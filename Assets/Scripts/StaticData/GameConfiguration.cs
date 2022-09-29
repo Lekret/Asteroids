20c10092f6f99b01cc8f670aa075a890e6d2260b
Lekret <lenicraft@mail.ru>
2022-09-29 04:50:13 +0300

    Configuration

---
+++ Assets/Scripts/StaticData/GameConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace StaticData
+{
+    [CreateAssetMenu(menuName = "StaticData/Game configuration", fileName = "GameConfiguration")]
+    public class GameConfiguration : ScriptableObject
+    {
+        [SerializeField] private ShipConfiguration _shipConfiguration;
+
+        public ShipConfiguration Ship => _shipConfiguration;
+    }
+}
---
+++ Assets/Scripts/StaticData/ShipConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using View;
+
+namespace StaticData
+{
+    [CreateAssetMenu(menuName = "StaticData/Ship configuration", fileName = "ShipConfiguration")]
+    public class ShipConfiguration : ScriptableObject
+    {
+        [SerializeField] private float _acceleration;
+        [SerializeField] private float _terminalAcceleration;
+        [SerializeField] private ShipView _prefab;
+
+        public float Acceleration => _acceleration;
+        public float TerminalSpeed => _terminalAcceleration;
+        public ShipView Prefab => _prefab;
+    }
+}
