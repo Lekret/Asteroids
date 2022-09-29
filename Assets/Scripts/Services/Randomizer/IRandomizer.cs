6473e3e3f290a4df921969f810761301ae130ad4
Lekret <lenicraft@mail.ru>
2022-09-29 19:35:34 +0300

    removed redundant

--- Assets/Scripts/Services/Randomizer/IRandomizer.cs
+++ Assets/Scripts/Services/Randomizer/IRandomizer.cs
@@ -4,7 +4,6 @@ namespace Services.Randomizer
 {
     public interface IRandomizer
     {
-        int Range(int min, int max);
         float Range(float min, float max);
     }
 
--- Assets/Scripts/Services/Randomizer/Randomizer.cs
+++ Assets/Scripts/Services/Randomizer/Randomizer.cs
@@ -4,11 +4,6 @@ namespace Services.Randomizer
 {
     public class Randomizer : IRandomizer
     {
-        public int Range(int min, int max)
-        {
-            return Random.Range(min, max);
-        }
-
         public float Range(float min, float max)
         {
             return Random.Range(min, max);
