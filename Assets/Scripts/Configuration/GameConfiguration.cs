1e031a8b43bcb459afbb8594f6883592faac2d37
Lekret <lenicraft@mail.ru>
2022-09-29 13:30:07 +0300

    minor changes

--- Assets/Scripts/Configuration/GameConfiguration.cs
+++ Assets/Scripts/Configuration/GameConfiguration.cs
@@ -7,20 +7,20 @@ namespace Configuration
     public class GameConfiguration : ScriptableObject,
         ISpawnConfiguration, IAsteroidConfiguration, IUfoConfiguration
     {
-        [SerializeField] private AnimationCurve _timeUntilSpawnCurve;
         [SerializeField] private float _ufoSpeed;
         [SerializeField] private float _bigAsteroidSpeed;
         [SerializeField] private float _smallAsteroidSpeed;
         [SerializeField] private float _asteroidLifetime;
         [SerializeField] private AsteroidView _asteroidPrefab;
         [SerializeField] private UfoView _ufoPrefab;
+        [SerializeField] private AnimationCurve _timeUntilSpawnCurve;
 
-        public AnimationCurve TimeUntilSpawnCurve => _timeUntilSpawnCurve;
         public float UfoSpeed => _ufoSpeed;
         public float BigAsteroidSpeed => _bigAsteroidSpeed;
         public float SmallAsteroidSpeed => _smallAsteroidSpeed;
         public float AsteroidLifetime => _asteroidLifetime;
         public AsteroidView AsteroidPrefab => _asteroidPrefab;
         public UfoView UfoPrefab => _ufoPrefab;
+        public AnimationCurve TimeUntilSpawnCurve => _timeUntilSpawnCurve;
     }
 }
