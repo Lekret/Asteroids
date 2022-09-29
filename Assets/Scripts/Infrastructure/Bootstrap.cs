dc9fdd7067d50c0bcc96d8c0ce4a8972120fecba
Lekret <lenicraft@mail.ru>
2022-09-29 04:50:08 +0300

    Bootstrap

---
+++ Assets/Scripts/Infrastructure/Bootstrap.cs
@@ -0,0 +1,22 @@
+using Game;
+using UnityEngine;
+using View;
+
+namespace Infrastructure
+{
+    public class Bootstrap : MonoBehaviour
+    {
+        private Ship _ship;
+        private ShipView _shipView;
+        
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+        
+        }
+    }
+}
